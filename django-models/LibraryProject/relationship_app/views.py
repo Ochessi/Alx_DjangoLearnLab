12b091211b6073ce2c08b6d630b685ef203507fb
Ochessi <ochessibrian@gmail.com>
2025-08-01 13:36:35 +0300

    Fixes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Book, Library
 
 # Function-based view
 def list_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()  # <- Required line for checker
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view
@@ -16,4 +12,3 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
