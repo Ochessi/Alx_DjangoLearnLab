af2226b26d42243326cb502ef4b8dc44f264b26e
Ochessi <ochessibrian@gmail.com>
2025-08-03 12:00:22 +0300

    Initial commit

---
+++ my_project/my_project/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for my_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from my_app import views
+from my_app.views import BookListCreateAPIView
+from django.shortcuts import redirect
+
+
+
+urlpatterns = [
+    path('', lambda request: redirect('book_list_create')),
+    path('admin/', admin.site.urls),
+    path("api/books", views.BookListCreateAPIView.as_view(), name="book_list_create"),
+]
