07545cddd8ef575502f4fcd13589beaa8b923fae
Ochessi <ochessibrian@gmail.com>
2025-08-09 16:12:28 +0300

    WIP: Save progress before rebase

--- advanced_api_project/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('authors/', views.AuthorListCreate.as_view(), name='author-list'),
-    path('authors/<int:pk>/', views.AuthorDetail.as_view(), name='author-detail'),
-    path('books/', views.BookListCreate.as_view(), name='book-list'),
-    path('books/<int:pk>/', views.BookDetail.as_view(), name='book-detail'),
-]
